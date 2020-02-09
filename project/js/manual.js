a964c0ec88a27e9f389228ebf2e00ac6bef05341
CreativeCoder9 <51862350+CreativeCoder9@users.noreply.github.com>
2020-02-09 19:23:10 +0530

    UIupdate

---
+++ project/js/manual.js
@@ -0,0 +1,17 @@
+document.getElementById("get-started-btn").onmouseover = function() {mouseOver()};
+document.getElementById("get-started-btn").onmouseout = function() {mouseOut()};
+document.getElementById("our-services-btn").onmouseover = function() {
+    document.getElementById("get-started-btn").style.background = "transparent";
+};
+document.getElementById("our-services-btn").onmouseout = function() {
+    document.getElementById("get-started-btn").style.background = "#323233";
+};
+
+
+function mouseOver() {
+    document.getElementById("our-services-btn").style.background = "transparent";
+}
+
+function mouseOut () {
+    document.getElementById("our-services-btn").style.backgroundColor = "#E73F3F"
+}
